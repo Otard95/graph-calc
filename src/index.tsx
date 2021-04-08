88067065c701bc3d303b72b2a905ffea378b9ea5
StianM <stian.myklebostad@schibsted.com>
2021-04-08 08:31:03 +0200

    Setup app base

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from './App'
+
+const appMount = document.querySelector('#app')
+
+if (appMount) {
+  render(<App/>, appMount)
+} else {
+  document.body.innerText = 'Failed to mount app'
+}
